21d1575d0ab317abbfe0b5e152ec099ebcc6b4f1
hayrobotics <cgrove689@gmail.com>
2019-04-02 16:13:25 -0700

    Set teleop controls for the forklift

--- forklift.py
+++ forklift.py
@@ -1,1 +1,22 @@
 # Forklift code!
+left_motor = ""
+right_motor = ""
+lift_motor = ""
+
+
+def autonomous_setup():
+    pass
+
+def autonomous_main():
+    pass
+
+def teleop_setup():
+    pass
+
+def teleop_main():
+    # If A is being pressed:
+    if Gamepad.get_value("button_a"):
+        Robot.set_value(lift_motor, "duty_cycle", 1)
+        # Move lift motor with positive pulse value
+    else:
+        Robot.set_value(lift_motor, "duty_cycle", 0)
